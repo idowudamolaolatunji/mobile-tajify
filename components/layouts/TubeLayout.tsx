7698de9bb2fa52a45d331c9a0924dca4addce2a8
idowudamolaolatunji <idowudamolaolatunji@gmail.com>
2025-01-17 11:08:52 +0100

    committing

--- components/layouts/TubeLayout.tsx
+++ components/layouts/TubeLayout.tsx
@@ -6,6 +6,9 @@ function TubeLayout({ video }: { video: any }) {
 	return (
 		<Pressable style={styles.videoCard}>
 			<Image source={{ uri: video.thumbnail }} style={styles.thumbnail} />
+            <View style={styles.videoTiming}>
+                <Text style={styles.videoTimingText}>10:00</Text>
+            </View>
 			<View style={styles.videoInfo}>
 				<Image source={{ uri: video.channel.avatar }} style={styles.channelAvatar} />
 				<View style={styles.textContainer}>
@@ -28,21 +31,39 @@ const styles = StyleSheet.create({
 		marginBottom: 12,
 		backgroundColor: variables.colors.card,
         borderRadius: 8,
-        overflow: "hidden"
+        overflow: "hidden",
+        position: "relative"
 	},
 	thumbnail: {
 		width: "100%",
-		aspectRatio: 16 / 8,
+		aspectRatio: 16 / 10,
 	},
+    videoTiming: {
+        backgroundColor: variables.colors.card,
+        position: "absolute",
+        maxWidth: 50,
+        paddingVertical: 2,
+        paddingHorizontal: 4,
+        borderRadius: 4,
+        bottom: "30%",
+        right: 5
+    },
+    videoTimingText: {
+        color: variables.colors.text
+    },
 	videoInfo: {
+        marginTop: -35,
 		flexDirection: "row",
-		padding: 6,
+		// paddingVertical: 15,
+        paddingTop: 8,
+        paddingBottom: 14,
+        paddingHorizontal: 8,
 		alignItems: "center",
 	},
 	channelAvatar: {
-		width: 28,
-		height: 28,
-		borderRadius: 14,
+        width: 36,
+        height: 36,
+		borderRadius: 50,
 		marginRight: 6,
 	},
 	textContainer: {
@@ -50,14 +71,13 @@ const styles = StyleSheet.create({
 		marginRight: 4,
 	},
 	videoTitle: {
-		fontSize: 12,
-		fontWeight: "500",
+		fontSize: 14,
 		color: variables.colors.text,
 		marginBottom: 1,
 		lineHeight: 16,
 	},
 	channelName: {
-		fontSize: 11,
+		fontSize: 12,
 		color: variables.colors.textSecondary,
 	},
 });
